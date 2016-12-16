06e8852a2477a28361e54e71dbcbac865f65e109
Joan Capdevila <capdevila.pujol.joan@gmail.com>
2016-12-16 11:07:53 +0100

    First Autodiff version for CAVI using Autograd

--- utils.py
+++ utils.py
@@ -1,6 +1,9 @@
 from scipy.special import psi, gammaln
 import numpy as np
 
+import autograd.numpy as agnp
+import autograd.scipy.special as agscipy 
+
 def dirichlet_expectation(alpha):
     if len(alpha.shape) == 1:
         return psi(alpha + np.finfo(np.float32).eps) - psi(np.sum(alpha))
@@ -11,3 +14,12 @@ def exp_normalize(aux):
 
 def log_beta_function(x):
     return np.sum(gammaln(x + np.finfo(np.float32).eps))-gammaln(np.sum(x + np.finfo(np.float32).eps))
+
+
+def ag_dirichlet_expectation(alpha):
+    if len(alpha.shape) == 1:
+        return agscipy.psi(alpha + agnp.finfo(np.float32).eps) - agscipy.psi(agnp.sum(alpha))
+    return agscipy.psi(alpha) - agscipy.psi(agnp.sum(alpha, 1))[:, agnp.newaxis]
+
+def ag_log_beta_function(x):
+    return agnp.sum(agscipy.gammaln(x + agnp.finfo(np.float32).eps))-agscipy.gammaln(agnp.sum(x + agnp.finfo(np.float32).eps))
