9a2ba9b77369d754cc8f6aa7b897e6bce6c14653
Alberto Pou <albertopouquiros@gmail.com>
2016-12-21 09:51:50 +0100

    tfInference

--- tfInference/coordAsc/gmm_means.py
+++ tfInference/coordAsc/gmm_means.py
@@ -11,9 +11,9 @@ from utils import tf_log_beta_function, tf_dirichlet_expectation, tf_dot
 
 parser = argparse.ArgumentParser(description='Inference in the gaussian mixture data with unknown means')
 parser.add_argument('-maxIter', metavar='maxIter', type=int, default=10)
-parser.add_argument('-K', metavar='K', type=int, default=2)
-parser.add_argument('-filename', metavar='filename', type=str, default="data_means.pkl")
-parser.add_argument('-alpha', metavar='alpha', nargs='+', type=float, default=[1.]*2)
+parser.add_argument('-K', metavar='K', type=int, default=4)
+parser.add_argument('-filename', metavar='filename', type=str, default='data_means.pkl')
+parser.add_argument('-alpha', metavar='alpha', nargs='+', type=float, default=[1.]*4)
 parser.add_argument('-m_o', metavar='m_o', nargs='+', type=float, default=[0., 0.])
 parser.add_argument('-beta_o', metavar='beta_o', nargs='+', type=float, default=0.01)
 parser.add_argument('-Delta_o', metavar='Delta_o', nargs='+', type=float, default=[1., 0., 0., 1.])
@@ -46,12 +46,14 @@ print('Delta_o shape: {}'.format(Delta_o.get_shape()))
 
 # Variable initialization
 phi_aux = np.random.dirichlet(alpha_aux, N)
+print('phi_aux: {}'.format(phi_aux))
 lambda_pi_aux = alpha_aux + np.sum(phi_aux, axis=0)
+print('lambda_pi_aux: {}'.format(lambda_pi_aux))
 lambda_mu_beta_aux = beta_o_aux + np.sum(phi_aux, axis=0)
-aux1 = np.tile(1./lambda_mu_beta_aux, (2, 1)).T
-aux2 = (beta_o_aux * m_o_aux + np.dot(phi_aux.T, data['xn']))
+print('lambda_mu_beta_aux: {}'.format(lambda_mu_beta_aux))
 lambda_mu_m_aux = np.tile(1./lambda_mu_beta_aux, (2, 1)).T * \
 				  (beta_o_aux * m_o_aux + np.dot(phi_aux.T, data['xn']))
+print('lambda_mu_m_aux: {}'.format(lambda_mu_m_aux))
 
 phi = tf.Variable(phi_aux, dtype=tf.float32)
 print('phi shape: {}'.format(phi.get_shape()))
@@ -65,7 +67,7 @@ print('lambda_mu_m shape: {}'.format(lambda_mu_m.get_shape()))
 # ELBO computation graph
 ELBO = tf.sub(tf_log_beta_function(lambda_pi), tf_log_beta_function(alpha))
 ELBO = tf.add(ELBO, 
-			  tf_dot(tf.sub(alpha, lambda_pi), tf_dirichlet_expectation(lambda_pi)))
+			  tf.matmul(tf.sub(alpha, lambda_pi), tf_dirichlet_expectation(lambda_pi)))
 ELBO = tf.add(ELBO,  
 			  tf.mul(K/2., tf.log(tf.matrix_determinant(tf.mul(beta_o, Delta_o)))))
 ELBO = tf.add(ELBO, 
@@ -74,8 +76,8 @@ ELBO = tf.add(ELBO,
 for k in xrange(K):
 
 	ELBO = tf.sub(ELBO,
-				  tf.mul(tf.div(beta_o, 2.), tf_dot(tf.sub(lambda_mu_m[k,:], m_o), 
-				  tf_dot(Delta_o, tf.transpose(tf.sub(lambda_mu_m[k,:], m_o))))))
+				  tf.mul(tf.div(beta_o, 2.), tf.matmul(tf.sub(lambda_mu_m[k,:], m_o), 
+				  tf.matmul(Delta_o, tf.transpose(tf.sub(lambda_mu_m[k,:], m_o))))))
 	ELBO = tf.sub(ELBO,
 				  tf.div(tf.mul(D, beta_o), tf.mul(2., lambda_mu_beta[k])))
 	ELBO = tf.sub(ELBO,
@@ -87,8 +89,8 @@ for k in xrange(K):
 		aux1 = tf.add(tf.sub(tf_dirichlet_expectation(lambda_pi)[k], tf.log(phi[n,k])), 
 					  tf.mul(1/2., tf.log(tf.div(tf.matrix_determinant(Delta_o), tf.mul(2., math.pi)))))
 		aux2 = tf.mul(1/2.,
-					  tf_dot(tf.sub(xn[n,:], lambda_mu_m[k,:]), 
-					 		 tf_dot(Delta_o, tf.transpose(tf.sub((xn[n,:], lambda_mu_m[k,:]))))))
+					  tf.matmul(tf.sub(xn[n,:], lambda_mu_m[k,:]), 
+					 		 tf.matmul(Delta_o, tf.transpose(tf.sub((xn[n,:], lambda_mu_m[k,:]))))))
 		ELBO = tf.add(ELBO,
 					  tf.mul(phi[n,k],
 					  		 tf.sub(tf.sub(aux1, aux2), tf.div(D, tf.mul(2., lambda_mu_beta[k])))))
