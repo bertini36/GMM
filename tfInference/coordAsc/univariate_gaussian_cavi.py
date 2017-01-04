e559d7379784bb1d2b1f31fc8ac436bc9cd9895f
Alberto Pou <albertopouquiros@gmail.com>
2017-01-04 17:10:05 +0100

    Summaries

--- tfInference/coordAsc/univariate_gaussian_cavi.py
+++ tfInference/coordAsc/univariate_gaussian_cavi.py
@@ -3,11 +3,10 @@
 import math
 import numpy as np
 import tensorflow as tf
-import matplotlib.pyplot as plt
-
-from utils import tf_log_beta_function, tf_dirichlet_expectation
 
 DEBUG = True
+SUMMARIES = True
+PRECISON = 0.0000001
 
 def printf(s):
     if DEBUG:
@@ -34,9 +33,9 @@ m_mu = tf.Variable(np.random.normal(0., (0.0001)**(-1.), 1)[0], dtype=tf.float64
 beta_mu_var = tf.Variable(np.random.gamma(a_gamma_ini, b_gamma_ini, 1)[0], dtype=tf.float64)
 
 # Maintain numerical stability
-a_gamma = tf.nn.softplus(a_gamma_var)
-b_gamma = tf.nn.softplus(b_gamma_var)
-beta_mu = tf.nn.softplus(beta_mu_var)
+a_gamma = tf.add(tf.nn.softplus(a_gamma_var), PRECISON)
+b_gamma = tf.add(tf.nn.softplus(b_gamma_var), PRECISON)
+beta_mu = tf.add(tf.nn.softplus(beta_mu_var), PRECISON)
 
 LB1 = tf.mul(tf.cast(1./2, tf.float64), tf.log(tf.div(beta, beta_mu)))
 LB2 = tf.add(LB1, tf.mul(tf.mul(tf.cast(1./2, tf.float64), tf.add(tf.pow(m_mu, 2), tf.div(tf.cast(1., tf.float64), beta_mu))), tf.sub(beta_mu, beta)))
@@ -60,13 +59,28 @@ LB15 = tf.sub(LB14, tf.mul(tf.div(tf.cast(N, tf.float64), tf.cast(2., tf.float64
 optimizer = tf.train.GradientDescentOptimizer(learning_rate=0.001)
 train = optimizer.minimize(LB15, var_list=[a_gamma_var, b_gamma_var, m_mu, beta_mu_var])
 
+# Summaries definition
+if SUMMARIES:
+    tf.summary.histogram('m_mu', m_mu)
+    tf.summary.histogram('beta_mu', beta_mu)
+    tf.summary.histogram('a_gamma', a_gamma)
+    tf.summary.histogram('b_gamma', b_gamma)
+    merged = tf.summary.merge_all()
+    file_writer = tf.summary.FileWriter('/tmp/tensorboard/', tf.get_default_graph())
+    run_calls = 0
+
 # Main program
 init = tf.global_variables_initializer()
 with tf.Session() as sess:
     sess.run(init)
-    for epoch in range(5):
+    for epoch in range(10):
         printf('***** Epoch {} *****'.format(epoch))
-        _, lb1, lb2, lb3, lb4, lb5, lb6, lb7, lb8, lb9, lb10, lb11, lb12, lb13, lb14, lb15 = sess.run([train, LB1, LB2, LB3, LB4, LB5, LB6, LB7, LB8, LB9, LB10, LB11, LB12, LB13, LB14, LB15])
+        if SUMMARIES:
+            _, m, lb1, lb2, lb3, lb4, lb5, lb6, lb7, lb8, lb9, lb10, lb11, lb12, lb13, lb14, lb15 = sess.run([train, merged, LB1, LB2, LB3, LB4, LB5, LB6, LB7, LB8, LB9, LB10, LB11, LB12, LB13, LB14, LB15])
+            run_calls += 1
+            file_writer.add_summary(m, run_calls)
+        else:
+            _, lb1, lb2, lb3, lb4, lb5, lb6, lb7, lb8, lb9, lb10, lb11, lb12, lb13, lb14, lb15 = sess.run([train, LB1, LB2, LB3, LB4, LB5, LB6, LB7, LB8, LB9, LB10, LB11, LB12, LB13, LB14, LB15])
         printf('ELBO1={}'.format(lb1))
         printf('ELBO2={}'.format(lb2))
         printf('ELBO3={}'.format(lb3))
