341892ca9c1e27ecad62f183468d6973cf4b618b
Alberto Pou <albertopouquiros@gmail.com>
2017-01-13 11:36:01 +0100

    tests/Mixture of Gaussians/gmm_means_elbo_tf OK

--- tests/Mixture of Gaussians/gmm_means_elbo_py.py
+++ tests/Mixture of Gaussians/gmm_means_elbo_py.py
@@ -15,43 +15,25 @@ def ELBO(xn, N, K, alpha, m_o, beta_o, Delta_o, lambda_pi, lambda_mu_m, lambda_m
 	ELBO += np.dot(alpha-lambda_pi, dirichlet_expectation(lambda_pi))
 	ELBO += K/2.*np.log(np.linalg.det(beta_o*Delta_o))
 	ELBO += K*D/2.
-	print('ELBO: {}'.format(ELBO))
-	# for k in xrange(K):
-	a1 = lambda_mu_m[1,:]-m_o
-	a2 = np.dot(Delta_o, (lambda_mu_m[1,:]-m_o).T)
-	a3 = beta_o/2.*np.dot(a1, a2)
-	a4 = D*beta_o/(2.*lambda_mu_beta[1])
-	a5 = 1/2.*np.log(np.linalg.det(lambda_mu_beta[1]*Delta_o))	
-	a6 = a3 + a4 + a5
-	ELBO -= a6
-	print('ELBO: {}'.format(ELBO))
-
-	b1 = phi[:,1].T
-	b2 = -np.log(phi[:,1])
-	b3 = 1/2.*np.log(np.linalg.det(Delta_o)/(2.*math.pi))
-	b4 = xn-lambda_mu_m[1,:]
-	b5 = np.dot(Delta_o,(xn-lambda_mu_m[1,:]).T)
-	b6 = 1/2.*np.diagonal(np.dot(b4, b5))
-	b7 = D/(2.*lambda_mu_beta[1])
-	b8 = b2+b3-b6-b7
-	print('b1: {}'.format(b1))
-	print('b8: {}'.format(b8))
-	print('b1*b8: {}'.format(np.dot(b1, b8)))
-	print('ELBO: {}'.format(ELBO))
-	ELBO += np.dot(b1, b8)
-
-	"""
-	for n in xrange(N):
-		b1 = phi[n,k]
-		b2 = dirichlet_expectation(lambda_pi)[k]
-		b3 = np.log(phi[n,k])
-		b4 = 1/2.*np.log(np.linalg.det(Delta_o)/(2.*math.pi))
-		b5 = xn[n,:]-lambda_mu_m[k,:]
-		b6 = np.dot(Delta_o, (xn[n,:]-lambda_mu_m[k,:]).T)
-		b7 = 1/2.*np.dot(b5, b6)
-		b8 = D/(2.*lambda_mu_beta[k])
-		ELBO += b1*(b2-b3+b4-b7-b8)
-	"""
+	for k in xrange(K):
+		a1 = lambda_mu_m[k,:]-m_o
+		a2 = np.dot(Delta_o, (lambda_mu_m[k,:]-m_o).T)
+		a3 = beta_o/2.*np.dot(a1, a2)
+		a4 = D*beta_o/(2.*lambda_mu_beta[k])
+		a5 = 1/2.*np.log(np.linalg.det(lambda_mu_beta[k]*Delta_o))	
+		a6 = a3 + a4 + a5
+		ELBO -= a6
+		b1 = phi[:,k].T
+		b2 = -np.log(phi[:,k])
+		b3 = 1/2.*np.log(np.linalg.det(Delta_o)/(2.*math.pi))
+		b4 = xn-lambda_mu_m[k,:]
+		b5 = np.dot(Delta_o,(xn-lambda_mu_m[k,:]).T)
+		b6 = 1/2.*np.diagonal(np.dot(b4, b5))
+		b7 = D/(2.*lambda_mu_beta[k])
+		b8 = b2+b3-b6-b7
+		print('ELBO={}'.format(ELBO))
+		ELBO += np.dot(b1, b8)
+		print('ELBO={}'.format(ELBO))
 	return ELBO
 
 def initialize(xn, K, alpha, m_o, beta_o, Delta_o):
--- tests/Mixture of Gaussians/gmm_means_elbo_tf.py
+++ tests/Mixture of Gaussians/gmm_means_elbo_tf.py
@@ -11,12 +11,12 @@ np.random.seed(7)
 sess = tf.Session()
 
 def tf_log_beta_function(x):
-	return tf.sub(tf.reduce_sum(tf.lgamma(tf.add(x, np.finfo(np.float64).eps))), \
-				  tf.lgamma(tf.reduce_sum(tf.add(x, np.finfo(np.float64).eps))))
+	return tf.sub(tf.reduce_sum(tf.lgamma(tf.add(x, np.finfo(np.float32).eps))), \
+				  tf.lgamma(tf.reduce_sum(tf.add(x, np.finfo(np.float32).eps))))
 
 def tf_dirichlet_expectation(alpha):
 	if len(alpha.get_shape()) == 1:
-		return tf.sub(tf.digamma(tf.add(alpha, np.finfo(np.float64).eps)), \
+		return tf.sub(tf.digamma(tf.add(alpha, np.finfo(np.float32).eps)), \
 					  tf.digamma(tf.reduce_sum(alpha)))
 	return tf.sub(tf.digamma(alpha), \
 				  tf.digamma(tf.reduce_sum(alpha, 1))[:, tf.newaxis])
@@ -50,6 +50,8 @@ lambda_pi = tf.Variable(lambda_pi_aux, dtype=tf.float64)
 lambda_mu_beta = tf.Variable(lambda_mu_beta_aux, dtype=tf.float64)
 lambda_mu_m = tf.Variable(lambda_mu_m_aux, dtype=tf.float64)
 
+xn = tf.convert_to_tensor(xn , dtype=tf.float64)
+
 # Reshapes
 lambda_mu_beta_res = tf.reshape(lambda_mu_beta, [K, 1])
 lambda_pi_res = tf.reshape(lambda_pi, [K, 1])
@@ -60,52 +62,28 @@ sess.run(init)
 # ELBO 
 ELBO = tf_log_beta_function(lambda_pi)
 ELBO = tf.sub(ELBO, tf_log_beta_function(alpha))
-ELBO = tf.add(ELBO, tf.matmul(tf.sub(alpha, lambda_pi), tf_dirichlet_expectation(lambda_pi_res)))
+ELBO = tf.add(ELBO, tf.matmul(tf.sub(alpha, lambda_pi), tf.reshape(tf_dirichlet_expectation(lambda_pi), [K,1])))
 ELBO = tf.add(ELBO, tf.mul(tf.cast(K/2., tf.float64), tf.log(tf.matrix_determinant(tf.mul(beta_o, Delta_o)))))
-ELBO = tf.add(ELBO, K*(D/2.))
-
-# for k in range(K):
-a1 = tf.sub(lambda_mu_m[1,:], m_o)
-a2 = tf.matmul(Delta_o, tf.transpose(tf.sub(lambda_mu_m[1,:], m_o)))
-a3 = tf.mul(tf.div(beta_o, 2.), tf.matmul(a1, a2))
-a4 = tf.div(tf.mul(tf.cast(D, tf.float64), beta_o), tf.mul(tf.cast(2., tf.float64), lambda_mu_beta_res[1]))
-a5 = tf.mul(tf.cast(1/2., tf.float64), tf.log(tf.mul(tf.pow(lambda_mu_beta_res[1], 2), tf.matrix_determinant(Delta_o))))
-a6 = tf.add(a3, tf.add(a4, a5))
-ELBO = tf.sub(ELBO, a6)
-print('ELBO: {}'.format(sess.run(ELBO)))
-
-b1 = tf.transpose(phi[:,1])
-b2 = tf.sub(tf.cast(0., tf.float64), tf.log(phi[:,1]))
-b3 = tf.mul(tf.cast(1/2., tf.float64), tf.log(tf.div(tf.matrix_determinant(Delta_o), tf.mul(tf.cast(2., tf.float64), math.pi))))
-b4 = tf.sub(xn, lambda_mu_m[1,:])
-b5 = tf.matmul(Delta_o, tf.transpose(tf.sub(xn, lambda_mu_m[1,:])))
-b6 = tf.mul(tf.cast(1/2., tf.float64), tf.pack([tf.matmul(b4, b5)[i,i] for i in range(N)]))
-b7 = tf.div(tf.cast(D, tf.float64), tf.mul(tf.cast(2., tf.float64), lambda_mu_beta[1]))
-b8 = tf.add(b2, tf.sub(b3, tf.sub(b6, b7)))
-print('ELBO: {}'.format(sess.run(ELBO)))
-
-b1 = tf.reshape(b1, [1,N])
-b8 = tf.reshape(b8, [1,N])
-print('b1: {}'.format(sess.run(b1)))
-print('b8: {}'.format(sess.run(b8)))
-prod = tf.matmul(b1, b8, transpose_b=True)
-print('b1*b8: {}'.format(sess.run(prod)))
-print('ELBO: {}'.format(sess.run(ELBO)))
-ELBO = tf.add(ELBO, prod)
-
-
-"""
-for n in range(N):
-	b1 = phi[n,k]
-	b2 = tf_dirichlet_expectation(lambda_pi)[k]
-	b3 = tf.log(phi[n,k])
-	b4 = tf.mul(1/2., tf.log(tf.div(tf.matrix_determinant(Delta_o), 2.*math.pi)))
-	b5 = tf.sub(xn[n,:], lambda_mu_m[k,:])
-	b6 = tf.matmul(Delta_o, tf.transpose(tf.sub(xn[n,:], lambda_mu_m[k,:])))
-	b7 = tf.mul(1/2., tf.matmul(b5, b6))
-	b8 = tf.div(tf.cast(D, tf.float64), tf.mul(2., lambda_mu_beta[1]))
-	ELBO = tf.add(ELBO, tf.mul(b1, tf.sub(b2, tf.add(b3, tf.sub(b4, tf.sub(b7, b8))))))
-"""
+ELBO = tf.add(ELBO, tf.cast(K*(D/2.), tf.float64))
+for k in range(K):
+	a1 = tf.sub(lambda_mu_m[k,:], m_o)
+	a2 = tf.matmul(Delta_o, tf.transpose(tf.sub(lambda_mu_m[k,:], m_o)))
+	a3 = tf.mul(tf.div(beta_o, 2.), tf.matmul(a1, a2))
+	a4 = tf.div(tf.mul(tf.cast(D, tf.float64), beta_o), tf.mul(tf.cast(2., tf.float64), lambda_mu_beta_res[k]))
+	a5 = tf.mul(tf.cast(1/2., tf.float64), tf.log(tf.mul(tf.pow(lambda_mu_beta_res[k], 2), tf.matrix_determinant(Delta_o))))
+	a6 = tf.add(a3, tf.add(a4, a5))
+	ELBO = tf.sub(ELBO, a6)
+	b1 = tf.transpose(phi[:,k])
+	b2 = tf.sub(tf.cast(0., tf.float64), tf.log(phi[:,k]))
+	b3 = tf.mul(tf.cast(1/2., tf.float64), tf.log(tf.div(tf.matrix_determinant(Delta_o), tf.mul(tf.cast(2., tf.float64), math.pi))))
+	b4 = tf.sub(xn, lambda_mu_m[k,:])
+	b5 = tf.matmul(Delta_o, tf.transpose(tf.sub(xn, lambda_mu_m[k,:])))
+	b6 = tf.mul(tf.cast(1/2., tf.float64), tf.pack([tf.matmul(b4, b5)[i,i] for i in range(N)]))
+	b7 = tf.div(tf.cast(D, tf.float64), tf.mul(tf.cast(2., tf.float64), lambda_mu_beta[k]))
+	b8 = tf.add(b2, tf.sub(b3, tf.sub(b6, b7)))
+	print('ELBO={}'.format(sess.run(ELBO)))
+	ELBO = tf.add(ELBO, tf.reduce_sum(tf.mul(b1, b8)))
+	print('ELBO={}'.format(sess.run(ELBO)))
 
 elbo = sess.run(ELBO)
 print('Final ELBO={}'.format(elbo))
