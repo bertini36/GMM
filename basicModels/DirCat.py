6d9f5ce007bd98de2411744553831c1bce1c0813
bertini36 <albertopouquiros@gmail.com>
2017-02-06 10:39:23 +0100

    NIW-Normal model Joan update

--- basicModels/DirCat.py
+++ basicModels/DirCat.py
@@ -10,16 +10,16 @@ from scipy.special import psi
 N = 1000
 K = 4
 
-## Data generation
+# Data generation
 alpha = np.array([20., 30., 10., 10.])
 pi = np.random.dirichlet(alpha)
 z = np.array([np.random.choice(K, 1, p=pi)[0] for n in xrange(N)])
 print('pi={}'.format(pi))
 
-## Prior definition
+# Prior definition
 alpha_prior = np.array([2., 3., 1., 1.])
 
-## Posterior computation
+# Posterior computation
 
 # Etas computation (with priors)
 etas = alpha_prior - 1.
--- basicModels/NIWNorm.py
+++ basicModels/NIWNorm.py
@@ -12,7 +12,7 @@ import matplotlib.pyplot as plt
 N = 1000
 D = 2
 
-## Data generation
+# Data generation
 
 # NIW Inverse Wishart hyperparameters
 v = 3.
@@ -22,7 +22,7 @@ sigma = invwishart.rvs(v, W)
 # NIW Normal hyperparameters
 m = np.array([1., 1.])
 k = 0.8
-mu = np.random.multivariate_normal(m, sigma*k)
+mu = np.random.multivariate_normal(m, sigma/k)
 
 print('mu={}'.format(mu))
 print('sigma={}'.format(sigma))
@@ -31,21 +31,23 @@ x = np.random.multivariate_normal(mu, sigma, N)
 plt.scatter(x[:, 0], x[:, 1], cmap=cm.gist_rainbow, s=5)
 plt.show()
 
-## Prior definition
+# Prior definition
 v_prior = 3.
-W_prior = np.array([[20., 30.], [25., 40.]])
-m_prior = np.array([1., 1.])
-k_prior = 0.8
+W_prior = np.array([[1., 0.], [0., 1.]])
+m_prior = np.array([0.5, 0.5])
+k_prior = 0.6
 
-## Posterior computation
+# Posterior computation
 k_pos = k_prior + N
 v_pos = v_prior + N
-m_pos = (((m_prior.T * k_prior) + np.sum(x)) / k_pos).T
-W_pos = W + np.dot(k_prior * m_prior, m_prior.T) + np.sum(np.dot(x, x.T)) - np.dot(k_pos * m_pos, m_pos.T)
+m_pos = (((m_prior.T * k_prior) + np.sum(x, axis=0)) / k_pos).T
+W_pos = W_prior + np.outer(k_prior * m_prior, m_prior) + \
+        np.sum(np.array([np.outer(x[n, :], x[n, :]) for n in xrange(N)]),
+               axis=0) - np.outer(k_pos * m_pos, m_pos)
 
-## Posterior sampling
+# Posterior sampling
 sigma_pos = invwishart.rvs(v_pos, W_pos)
-mu_pos = np.random.multivariate_normal(m_pos, sigma_pos*k_pos)
+mu_pos = np.random.multivariate_normal(m_pos, sigma_pos/k_pos)
 
 print('Inferred mu={}'.format(mu_pos))
 print('Inferred sigma={}'.format(sigma_pos))
