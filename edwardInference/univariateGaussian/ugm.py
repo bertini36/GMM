375d44124dd5524e4c6252e1a00220f6e5fab509
bertini36 <albertopouquiros@gmail.com>
2017-02-17 11:42:46 +0100

    UGM con Edward no va por inestabilidad de la Gamma con BBVI

--- edwardInference/univariateGaussian/ugm.py
+++ edwardInference/univariateGaussian/ugm.py
@@ -6,44 +6,45 @@ process to approximate an univariate gaussian
 """
 
 import edward as ed
+import matplotlib.pyplot as plt
 import numpy as np
 import tensorflow as tf
 from edward.models import Gamma, Normal
 
-N = 100
-
+N = 1000
 ed.set_seed(42)
 
-# Model hyperparameters
-m = [0.]
-beta = [0.0001]
-a = [0.001]
-b = [0.001]
-
-# Probabilistic model
-mu = Normal(mu=m, sigma=beta)
-sigma = Gamma(alpha=a, beta=b)
-x = Normal(mu=ed.dot(tf.ones([N, 1]), mu), sigma=ed.dot(tf.ones([N, 1]), sigma))
-
-# Variational model definition
-lambda_mu_m = tf.Variable(tf.random_normal([1]))
-lambda_mu_beta = tf.nn.softplus(tf.Variable(tf.zeros([1])))
-lambda_a = tf.Variable(tf.random_gamma([1], 1))
-lambda_b = tf.Variable(tf.random_gamma([1], 1))
-qmu = Normal(mu=lambda_mu_m, sigma=lambda_mu_beta)
-qsigma = Gamma(alpha=lambda_a, beta=lambda_b)
-
 # Data generation
-xn = np.random.normal(7, 1, N)
-
+xn = np.random.normal(7, 1, [N])
+plt.plot(xn, 'go')
+plt.title('Simulated dataset')
+plt.show()
 print('mu=7')
 print('sigma=1')
 
+# Priors definition
+m = tf.Variable([0.], trainable=False)
+beta = tf.Variable([0.0001], trainable=False)
+a = tf.Variable([0.001], trainable=False)
+b = tf.Variable([0.001], trainable=False)
+
+# Posterior inference
+# Probabilistic model
+mu = Normal(mu=m, sigma=beta)
+sigma = Gamma(alpha=a, beta=b)
+x = Normal(mu=tf.tile(mu, [N]), sigma=tf.tile(sigma, [N]))
+
+# Variational model
+qmu = Normal(mu=tf.Variable(tf.random_normal([1])),
+             sigma=tf.nn.softplus(tf.Variable(tf.random_normal([1]))))
+qsigma = Gamma(alpha=tf.nn.softplus(tf.Variable(tf.random_normal([1]))),
+               beta=tf.nn.softplus(tf.Variable(tf.random_normal([1]))))
+
 # Inference
 inference = ed.KLqp({mu: qmu, sigma: qsigma}, data={x: xn})
-inference.run(n_samples=30, n_iter=40000)
+inference.run(n_iter=1500, n_samples=500)
 
 sess = ed.get_session()
 
-print('Inferred mu={}'.format(sess.run(qmu.value())))
-print('Inferred sigma={}'.format(sess.run(qsigma.value())))
+print('Inferred mu={}'.format(sess.run(qmu.mean())))
+print('Inferred sigma={}'.format(sess.run(qsigma.mean())))
