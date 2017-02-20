2379e9b7b86b50c950014780d924608a5c49c320
bertini36 <albertopouquiros@gmail.com>
2017-02-20 16:10:49 +0100

    Inv.Gamma-Normal model Python

---
+++ basicModels/invgamma_normal.py
@@ -0,0 +1,29 @@
+# -*- coding: UTF-8 -*-
+
+"""
+InvGamma-Normal Model
+Posterior exact inference
+"""
+
+import numpy as np
+from scipy.stats import invgamma
+
+N = 1000
+
+# Data generation (known mean)
+mu = 5
+sigma = invgamma.rvs(1, scale=0.7)
+x = np.random.normal(mu, sigma, N)
+
+print('sigma={}'.format(sigma))
+
+# Prior definition
+alpha = 1
+beta = 0.6
+
+# Posterior computation
+alpha_pos = alpha + (N / 2)
+beta_pos = beta + (np.sum(np.power(x - mu, 2)) / 2)
+sigma_pos = np.sqrt(invgamma.rvs(alpha_pos, scale=beta_pos))
+
+print('Inferred sigma={}'.format(sigma_pos))
