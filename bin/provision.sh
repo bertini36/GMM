eae47d29d8b297b223cce45a8c7e53e21c797d50
bertini36 <albertopouquiros@gmail.com>
2017-04-27 12:15:12 +0200

    Cosas

--- bin/provision.sh
+++ bin/provision.sh
@@ -1,14 +1,19 @@
-#!/bin/bash
-echo "Installing virtualenvwrapper in case it is not installed"
-sudo pip install virtualenvwrapper
+#!/usr/bin/env bash
+
+# Execution: ./provision.sh
+
 echo "Configuring environment variables"
-source bin/start-env.sh
-echo "Copying files"
-if [ -z "$VIRTUAL_ENV" ]; then
-    echo "Creating virtual environment $VIRTUAL_ENV"
-    #echo "Really making"
-    mkvirtualenv $PROJECT
-fi
+export PROJECT=GMM2
+export WORKON_HOME=~/.virtualenvs
+
+echo "Installing virtualenv and virtualenvwrapper in case it is not installed"
+sudo pip install virtualenv
+sudo pip install virtualenvwrapper
+
+echo "Creating virtual environment"
+. `which virtualenvwrapper.sh`
+mkvirtualenv $PROJECT
+workon $PROJECT
+
 echo "Installing packages in virtual environment"
 pip install -r requirements.txt
-pip install --upgrade $TF_BINARY_URL
